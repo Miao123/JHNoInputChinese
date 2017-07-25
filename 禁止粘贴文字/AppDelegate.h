9d875a23bcb0b60ac1821fedd4ee1a83e558cd95
Miao123 <miao_ios@163.comc>
2017-07-25 17:52:23 +0800

    Initial Commit

---
+++ 禁止粘贴文字/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  禁止粘贴文字
+//
+//  Created by 苗建浩 on 2017/7/25.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 禁止粘贴文字/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  禁止粘贴文字
+//
+//  Created by 苗建浩 on 2017/7/25.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
